3f9e8d9f0ef1d706c3c2e548410be2c45fa40dde
Valerija Prohorenkova <valerijaprohorenkova@Valerijas-MBP.fios-router.home>
2020-04-13 22:14:40 -0400

    haha javascript

--- public/javascripts/javastuff.js
+++ public/javascripts/javastuff.js
@@ -1,1 +1,9 @@
-console.log("test");
+function toggleDark() {
+  let body = document.body;
+
+  if (body.classList.contains("darkmode")){
+    body.classList.remove("darkmode");
+  } else {
+    body.classList.add("darkmode");
+  }
+}
