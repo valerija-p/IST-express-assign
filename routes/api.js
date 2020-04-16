864bb3fe4e7f6f4deb5b8e5fc95d012f45d8e2c4
Connor Schwenk <cschwenk@Connors-MBP.fios-router.home>
2020-04-16 19:25:09 -0400

    @@@@

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/",function(request,response,next){
- response.send("you hit the api route!");
+router.get('/',function(request,response,next){
+  response.send('You hit the api route');
+});
+
+router.get('/endpoint1',function(request,response,next){
+  response.send('You hit the endpoint 1 route');
 });
 
 module.exports = router;
